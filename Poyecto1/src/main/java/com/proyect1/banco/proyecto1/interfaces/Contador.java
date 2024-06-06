da764805cc51fac654f36d44c7dc039cbdc1b2d5
Andres Varela <agvarela@bancolombia.com.co>
2024-06-06 00:42:05 -0500

    Se agregan las actividades de inferfaces y fechasHorarios

---
+++ Poyecto1/src/main/java/com/proyect1/banco/proyecto1/interfaces/Contador.java
@@ -0,0 +1,22 @@
+package com.proyect1.banco.proyecto1.interfaces;
+
+public class Contador implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("El hilo fue interrumpido");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread hilo = new Thread(new Contador());
+        hilo.start();
+    }
+}
+
